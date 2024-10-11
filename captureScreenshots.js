95c3bccfd0f2d51540515ae8203fb52cec1e5d0a
darko <pegaz.int@siol.net>
2024-10-11 11:32:28 +0200

    Action stop working

--- captureScreenshots.js
+++ captureScreenshots.js
@@ -3,6 +3,7 @@ const puppeteer = require('puppeteer');
 (async () => {
 const browser = await puppeteer.launch({
   headless: 'new',
+  args: ['--no-sandbox', '--disable-setuid-sandbox'] 
 });	
   const page = await browser.newPage();
   await page.emulateTimezone('Europe/Ljubljana')
